90da2ac651d1e93d175b6c0fe0adf4c751dfa45d
Kester Yeo <40845783+EchoKes@users.noreply.github.com>
2020-04-26 14:11:11 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-public class Main
+public class Question5
 {
   public static void main(String[] args)
   {
